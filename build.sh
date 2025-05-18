8c6059e420ee9ec1cfc7331ebc6087cbc971cc21
rjmango <you@example.comgit config --global user.email you@example.com>
2025-05-18 17:50:27 +0800

    modified build command

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ wget -O "$(node -p "require('os').homedir()")/.u2net/u2net.onnx" \
      https://github.com/danielgatis/rembg/releases/download/v0.0.0/u2net.onnx
 
 # Verify download succeeded
-if [ ! -f "$(node -p "require('os').homedir()")/u2net.onnx" ]; then
+if [ ! -f "$(node -p "require('os').homedir()")/.u2net/u2net.onnx" ]; then
     echo "Error: File download failed!"
     exit 1
 fi
